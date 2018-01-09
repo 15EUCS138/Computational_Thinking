e170ae8f8d2ff85c7663978c3d47c4a3d29b8ce4
15EUCS138 <34640771+15EUCS138@users.noreply.github.com>
2018-01-09 07:08:55 +0000

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+  int n,a[10],i;
+  printf("Enter the number to convert:");
+  scanf("%d",&n);
+  
+  for(i=0;n>0;i++)
+  {
+  	a[i]=n%2;
+  	n=n/2;
+  }
+ printf("The binary number is:");
+  for(i=i-1;i>=0;i--)
+  {
+    printf("%d",a[i]);
+  	
+  }
+  
+  return 0;
+}
+  
